e228b59fff2cc8a7d4c18591118364570857c0e0
andrey <andrey.onoue@banqi.com>
2022-10-31 15:01:05 -0300

    Subindo

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    projectId: "fsgi6q",
     viewportHeight: 1920,
     viewportWidth: 1080,
     baseUrl: 'https://buger-eats-qa.vercel.app'
